d3b18ffa86199b489477614c5ff7ef58a9aa4539
Izhar Hussain <hussain786izhar@gmail.com>
2020-02-23 17:12:24 +0530

    FunctionalInterface

---
+++ Java8Test/src/FunctionalInterface/functionalInterface/Funtionally.java
@@ -0,0 +1,10 @@
+package com.app.learn.Java8learn.functionalInterface;
+
+public interface Funtionally {
+	
+	public static void main (String [] args)
+	{
+		System.out.println("Interface can be run by static methods");
+	}
+
+}
